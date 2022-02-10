ba2fb483721e6ca8a7c269ddd4a416c36903ec95
DafneBascunan <dafnepaola.b@gmail.com>
2022-02-10 08:39:20 -0300

    hover imagenes y boton volver

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import App from './components/App.js';
 
 document.getElementById('root').appendChild(App())
 
-// let userName = document.getElementById("nameInput");
-// let userNamePrint = document.getElementById("userNamePrint");
-    
-// userName.addEventListener("keyup", function(){
-//          userNamePrint.innerHTML = userName.value;
-//      })
+let nombre = window.prompt ("Ingresa tu nombre");
+
+alert("Hola, " + nombre );
+
 
